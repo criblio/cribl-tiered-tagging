3e2b6ef441fb76f80d8b312b98b031a46f6ed117
unknown <kgeorge13@gmail.com>
2022-06-03 14:30:12 -0700

    FIX: Nesting in engine.rb

--- lib/cribl-tiered-tagging/engine.rb
+++ lib/cribl-tiered-tagging/engine.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 module ::CriblTieredTagging
-    class Engine < ::Rails::Engine
-      engine_name "cribl_tiered_tagging".freeze
-      isolate_namespace CriblTieredTagging
-    end
+  class Engine < ::Rails::Engine
+    engine_name "cribl_tiered_tagging".freeze
+    isolate_namespace CriblTieredTagging
   end
-  
+end
