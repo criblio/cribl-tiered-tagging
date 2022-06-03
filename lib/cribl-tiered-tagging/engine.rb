46ca989461e983218b4679064da72073e3d1101a
unknown <kgeorge13@gmail.com>
2022-06-03 14:28:44 -0700

    DEV: Simplify plugin.rb and add other base files

---
+++ lib/cribl-tiered-tagging/engine.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ::CriblTieredTagging
+    class Engine < ::Rails::Engine
+      engine_name "cribl_tiered_tagging".freeze
+      isolate_namespace CriblTieredTagging
+    end
+  end
+  
